8cd52bab323d909074aa9b3354fc8f337ec0e743
Harish Bohara <harish.bohara@gmail.com>
2020-11-30 00:49:04 +0530

    [x] added suport to build rocks-db cloud

---
+++ Formula/rocksdb-cloud-s3.rb
@@ -0,0 +1,36 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class RocksDbCloudS3 < Formula
+  desc "Build RocksDB-Cloud"
+  homepage "https://github.com/harishb2k/rocksdb-cloud"
+  license ""
+  head "https://github.com/harishb2k/rocksdb-cloud.git", :tag => "0.0.1"
+
+  depends_on "cmake" => :build
+  depends_on "openssl"
+  depends_on "glog"
+  
+  stable do
+    url "https://github.com/apache/rocksdb-cloud.git", :using => :git, :tag => "0.0.1"
+  end
+    
+  def install
+    system "cmake", ".", *std_cmake_args
+    
+    system "make", "-j4", "install" 
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test incubator-brpc`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
