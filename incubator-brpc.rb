abb2054da2be6ef4a057b7ce0e235a1fbfaf8871
Harish Bohara <harish.bohara@gmail.com>
2020-11-29 22:33:49 +0530

    [x] Adding formula to build BRPC

---
+++ incubator-brpc.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class IncubatorBrpc < Formula
+  desc ""
+  homepage ""
+  license ""
+  head "https://github.com/apache/incubator-brpc.git"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # system "cmake", "cmake . -DWITH_GLOG=ON", "--prefix=#{prefix}"
+    system "cmake", ".", "-DWITH_GLOG=ON", *std_cmake_args
+    
+    system "make", "install"	
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test incubator-brpc`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
